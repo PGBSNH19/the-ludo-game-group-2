4b53e64b30f018084ab824b82dbd199195215e8f
Adgnascor <william.lindahl@live.com>
2020-04-01 12:07:40 +0200

    Update Game.cs

--- Source/GameEngine/Models/Game.cs
+++ Source/GameEngine/Models/Game.cs
@@ -6,22 +6,28 @@ using System.Text;
 
 namespace GameEngine.Library.Models
 {
-    public class Game: IGame, IUserTurn
+    public class Game : IGame, IUserTurn
     {
         public IUserTurn UserTurn { get; set; }
 
         public int GameID { get; set; }
-        public User User { get; set; }
-        public List<User> Users { get; set; }
-        public int GameBoardID { get; set; }
-        public GameBoard GameBoard { get; set; }
-        public Dice Dice { get; set; }
+        public User User { get => user; set => user = value; }
+        public List<User> Users { get => users; set => users = value; }
+        public int GameBoardID { get => gameBoardID; set => gameBoardID = value; }
+        public GameBoard GameBoard { get => gameBoard; set => gameBoard = value; }
+        public Dice Dice { get => dice; set => dice = value; }
 
         //Use private variables to be able to reach them in the below methods. 
         // Step 1. Create private variables for every instance  you need.
         // Step 2. Instansiate every class that will be used as a parameter.
         // Step 3. Dont forget to change below code to private variables.
-        private int diceResult;
+        private int diceResult; // ta bort denna?
+        private User user;
+        private List<User> users;
+        private int gameBoardID;
+        private GameBoard gameBoard;
+        private Dice dice;
+
         public Game(int numberOfPlayers)
         {
             Users = User.GetPlayersAndName(numberOfPlayers);
